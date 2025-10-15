c650682754c2fab143404dfbb75341541e009db7
clarkisa <clarisa.vasquez@ucb.edu.bo>
2025-10-15 17:02:17 -0400

    Archivo de Tiva ej3 vision

---
+++ buen_robotv43/gcc/buen_robotv43.d
@@ -0,0 +1,8 @@
+gcc/buen_robotv43.o: buen_robotv43.c \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h \
+ /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdbool.h \
+ ../../../../inc/hw_memmap.h ../../../../inc/hw_ints.h \
+ ../../../../driverlib/sysctl.h ../../../../driverlib/gpio.h \
+ ../../../../driverlib/pwm.h ../../../../driverlib/pin_map.h \
+ ../../../../driverlib/interrupt.h ../../../../driverlib/uart.h \
+ ../../../../driverlib/timer.h ../../../../driverlib/rom_map.h
